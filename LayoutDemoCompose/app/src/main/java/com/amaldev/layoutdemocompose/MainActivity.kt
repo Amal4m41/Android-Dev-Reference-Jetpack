0ef6217469ae199394d589a09669af4c85be5732
Amal4m41 <amalsunil117@gmail.com>
2021-09-27 11:57:45 +0530

    Added constraint layout for compose demo

--- LayoutDemoCompose/app/src/main/java/com/amaldev/layoutdemocompose/MainActivity.kt
+++ LayoutDemoCompose/app/src/main/java/com/amaldev/layoutdemocompose/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedContentScope.SlideDirection.Companion.End
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import com.amaldev.layoutdemocompose.ui.theme.LayoutDemoComposeTheme
 import kotlinx.coroutines.launch
 
@@ -19,59 +26,109 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 
-            val scaffoldState = rememberScaffoldState()
+//            TextFieldButtonSnackbarDemo()
+
+            //We define the id for the elements we want to constraint, and the constraints we want on them.
+            val constraints = ConstraintSet {
+                val greenBox = createRefFor("greenBox")
+                val redBox = createRefFor("redBox")
+
+                val guideline = createGuidelineFromTop(0.5f)  //take 50 percent of the screen from top
+
+                //defining the constraint
+                constrain(greenBox){
+//                    this.top.linkTo(parent.top)
+                    this.top.linkTo(guideline)
+                    start.linkTo(parent.start)
+
+                    width= Dimension.value(100.dp)
+                    height= Dimension.value(100.dp)
+                }
+
+                constrain(redBox){
+                    top.linkTo(parent.top)
+                    start.linkTo(greenBox.end)
+                    end.linkTo(parent.end)
+                    width= Dimension.value(100.dp)
+//                    width= Dimension.fillToConstraints  //similar to 0dp width
+                    height= Dimension.value(100.dp)
+                }
+
+                createHorizontalChain(greenBox,redBox,chainStyle = ChainStyle.Packed)
+
+            }
+
+            ConstraintLayout(constraints,modifier = Modifier.fillMaxSize()) {
+
+                Box(modifier = Modifier
+                    .background(Color.Green)
+                    .layoutId("greenBox"))  //assigning the id
+
+                Box(modifier = Modifier
+                    .background(Color.Red)
+                    .layoutId("redBox"))
+
+            }
+        }
+    }
+}
+
+
+@Composable
+fun TextFieldButtonSnackbarDemo(){
+
+    val scaffoldState = rememberScaffoldState()
 //            val textFieldState = remember{   //can be accessed by textFieldState.value
 //                mutableStateOf("")
 //            }
 
-            var textFieldState by remember{  //we can directly access by using textFieldState
-                mutableStateOf("")
-            }
+    var textFieldState by remember{  //we can directly access by using textFieldState
+        mutableStateOf("")
+    }
 
-            val scope = rememberCoroutineScope()
+    val scope = rememberCoroutineScope()
 
-            Scaffold(
-                modifier = Modifier.fillMaxSize(),
-                scaffoldState = scaffoldState
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(horizontal = 50.dp)
-                ) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        scaffoldState = scaffoldState
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 50.dp)
+        ) {
 
-                    TextField(
-                        value = textFieldState,
-                        onValueChange = {
-                            textFieldState = it //will assign the state and recompose all the composables that has to do with this state.
-                        } ,
-                        label = {
-                            Text(text = "Enter Name")  //hint text
-                        },
-                        singleLine = true,
-                        modifier = Modifier.fillMaxWidth()
-                    )
+            TextField(
+                value = textFieldState,
+                onValueChange = {
+                    textFieldState = it //will assign the state and recompose all the composables that has to do with this state.
+                } ,
+                label = {
+                    Text(text = "Enter Name")  //hint text
+                },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
 
-                    Spacer(modifier = Modifier.height(15.dp))
-                    Button(onClick = {
-                        scope.launch {
-                            scaffoldState.snackbarHostState.showSnackbar("Hello ${textFieldState}")
-                        }
-                    },
-                        modifier = Modifier.align(Alignment.End)
-                    ) {
-                        Text("Click me!")
-                    }
-
+            Spacer(modifier = Modifier.height(15.dp))
+            Button(onClick = {
+                scope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar("Hello ${textFieldState}")
                 }
+            },
+                modifier = Modifier.align(Alignment.End)
+            ) {
+                Text("Click me!")
             }
-
 
         }
     }
+
 }
+
+
 
 @Composable
 fun Greeting(name: String) {
